c4ab4864982c68ef13a33c2fdb8ac22696cf84d2
dimansidorov <dimkasidorow@gmail.com>
2023-09-20 20:47:01 +0200

    Updated AdvertRetrieveAPIView

--- src/advert/views.py
+++ src/advert/views.py
@@ -1,5 +1,4 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.response import Response
 
 
 from .models import Advert
@@ -17,9 +16,8 @@ class AdvertRetrieveAPIView(RetrieveAPIView):
     
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
-
         instance.views += 1
         instance.save()
 
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+        return super().retrieve(self, request, *args, **kwargs)
+
