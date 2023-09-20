1c6641f3311913107f467ac952da47233d8c6e64
dimansidorov <dimkasidorow@gmail.com>
2023-09-20 20:38:19 +0200

    Added AdvertListAPIView, AdvertRetrieveAPIView

--- src/advert/views.py
+++ src/advert/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+from .models import Advert
+from .serializers import AdvertModelSerializer
+
+
+class AdvertListAPIView(ListAPIView):
+    queryset = Advert.objects.all()
+    serializer_class = AdvertModelSerializer
+
+
+class AdvertRetrieveAPIView(RetrieveAPIView):
+    queryset = Advert.objects.all()
+    serializer_class = AdvertModelSerializer
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        instance.views += 1
+        instance.save()
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
