1a39f519d1e68a977359fed3b02168a996b1b751
dimansidorov <dimkasidorow@gmail.com>
2023-09-20 17:15:49 +0200

    create models in Advert app: Category, City, Advert

---
+++ src/advert/models.py
@@ -0,0 +1,27 @@
+from django.db import models 
+from django.core.validators import MinValueValidator 
+
+
+class Category(models.Model): 
+    name = models.CharField(max_length=50, null=False) 
+    
+    def __str__(self) -> str: 
+        return self.name 
+    
+    
+class City(models.Model): 
+    name = models.CharField(max_length=50, null=False) 
+    
+    def __str__(self) -> str: 
+        return self.name 
+    
+    
+class Advert(models.Model): 
+    title = models.CharField(max_length=50, null=False) 
+    description = models.TextField(blank=False, null=False) 
+    city = models.ForeignKey(City, on_delete=models.PROTECT, null=False) 
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=False) 
+    views = models.BigIntegerField(default=0, validators=[MinValueValidator(0)]) 
+    
+    def __str__(self) -> str: 
+        return self.title
