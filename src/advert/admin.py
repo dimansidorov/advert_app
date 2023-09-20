cdb2dc51325e119b00fb31389a31f12f4e56ca58
dimansidorov <dimkasidorow@gmail.com>
2023-09-20 17:23:38 +0200

    Advert app: updated admin.py

--- src/advert/admin.py
+++ src/advert/admin.py
@@ -1,1 +1,19 @@
+from django.contrib import admin 
+from .models import Category, City, Advert 
 
+
+class CategoryAdmin(admin.ModelAdmin): 
+    pass 
+
+
+class CityAdmin(admin.ModelAdmin): 
+    pass 
+
+
+class AdvertAdmin(admin.ModelAdmin): 
+    pass 
+
+
+admin.site.register(Category, CategoryAdmin) 
+admin.site.register(City, CityAdmin) 
+admin.site.register(Advert, AdvertAdmin)
